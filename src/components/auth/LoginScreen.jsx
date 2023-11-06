bd5cf197fac61cbcb65c84da1d523bc9e7fbbb8d
J-Ayerbe <wesker0238@protonmail.com>
2023-11-06 03:00:47 -0500

    Add package dependencies for @emotion/react, @emotion/styled, and @mui/material. Add Snackbar component to display login success/error messages.

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { loginFunction } from '../../hooks/mutations/useLogin'
 import { Formik, ErrorMessage, Field, Form } from 'formik'
 import { loginSchema } from '../../helpers/schemas/loginSchema'
 import { useNavigate } from 'react-router-dom'
+import Snackbar from '@mui/material/Snackbar'
 
 const LoginScreen = () => {
+  const [open, setOpen] = useState(false)
+  const [messageSnackbar, setMessageSnackbar] = useState('second')
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setOpen(false)
+  }
   const navigate = useNavigate()
   const loginMutation = useMutation({
     mutationFn: loginFunction,
     onSuccess: (data) => {
       console.log(data)
+      setOpen(true)
       navigate('/')
     },
     onError: (error) => {
       console.log(error)
+      setMessageSnackbar(error.message)
+      setOpen(true)
     }
   })
 
@@ -101,8 +114,10 @@ const LoginScreen = () => {
               <div>
                 <button
                   type="submit"
-                  disabled={isSubmitting}
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  disabled={loginMutation.isPending}
+                  className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                    loginMutation.isPending ? 'opacity-50' : ''
+                  }`}
                 >
                   Iniciar sesión
                 </button>
@@ -111,6 +126,12 @@ const LoginScreen = () => {
           )}
         </Formik>
       </div>
+      <Snackbar
+        open={open}
+        autoHideDuration={1700}
+        onClose={handleClose}
+        message={messageSnackbar}
+      />
     </div>
   )
 }
