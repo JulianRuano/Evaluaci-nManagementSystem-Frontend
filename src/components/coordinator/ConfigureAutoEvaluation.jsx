2a58d6ff16b7c130f3f856c95dd0b0092e56acf7
Brayan Majin Gaviria <brayannmajin@gmail.com>
2023-11-16 14:53:03 -0500

    added changes

--- src/components/coordinator/ConfigureAutoEvaluation.jsx
+++ src/components/coordinator/ConfigureAutoEvaluation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ConfigureAutoEvaluation = () => {
-  return <div>ConfigureAutoEvaluation</div>
+               return <div>ConfigureAutoEvaluation</div>
 }
 
 export default ConfigureAutoEvaluation
