ef72762a00e94905c7b08cfa7c57e7d87f5cedc1
JulianRuano <julianrmajin@gmail.com>
2023-11-25 15:49:59 -0500

    Add dependencies and scripts

--- src/hooks/mutations/useLogin.js
+++ src/hooks/mutations/useLogin.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { apiUrl } from '../../helpers/apiUrl'
 
+
 export async function loginFunction(body) {
   const config = {
     withCredentials: true
   }
   const response = await axios.post(`${apiUrl}/auth/login`, body, config)
+  
   return response.data
 }
